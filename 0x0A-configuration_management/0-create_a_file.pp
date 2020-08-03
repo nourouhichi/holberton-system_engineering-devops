a23208c7fd34ffc75ccf1a4447896b4166289134
nourouhichi <1417@holbertonschool.com>
2020-08-03 23:05:13 +0100

    fix indentation

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 #create a file
 file { 'holberton':
-path => '/tmp/holberton',
-mode => '0744',
-owner => 'www-data',
-group => 'www-data',
+path    => '/tmp/holberton',
+mode    => '0744',
+owner   => 'www-data',
+group   => 'www-data',
 content => 'I love Puppet'
 }
