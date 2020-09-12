645603810ec23d190a019a8b2c0206ee0667052b
nourouhichi <1417@holbertonschool.com>
2020-09-12 15:33:25 +0100

    correct

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #task 0 with puppet
-exec {'custom gttp header':
+exec {'http header':
 command  => 'sudo apt-get update
 sudo apt-get install nginx
 sudo sed -i "11i\\\tadd_header X-Served-By $HOSTNAME;" /etc/nginx/nginx.conf
