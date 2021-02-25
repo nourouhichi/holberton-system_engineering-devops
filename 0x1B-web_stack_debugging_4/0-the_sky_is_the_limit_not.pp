5c637da2d8f6fd58b901480477b8626ef188266c
nourouhichi <1417@holbertonschool.com>
2021-02-25 21:27:18 +0100

    new add

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,1 +1,10 @@
-#comment goes  here
+#comment goes here
+
+exec{ 'fix_nginx':
+      command  => 'sed -i "s/-n 15/-n 4100/g" /etc/default/nginx',
+      provider => 'shell'
+}
+exec{'restart_nginx':
+  command  => 'sudo service nginx restart',
+  provider => 'shell'
+}
--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,1 +1,5 @@
 #comment goes  here
+exec {'UserLimit':
+  command => 'sed -i "s/nofile 5/no file 4096/; s/nofile 4/nofile 1024/" /etc/security/limits.conf',
+  path    => ['/usr/bin', '/bin'],
+}
